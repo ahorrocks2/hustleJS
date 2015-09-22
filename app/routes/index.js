aca31c28f63d0e350ea16d0317ba853e3300cfe9
Alyssa Horrocks & Kendra Ash <student@epicodus.com>
2015-09-22 14:56:23 -0700

    Add carousel base.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
 
-$(document).ready(function() {
-  var t = setInterval(function() {
-    $("#carousel ul").animate({marginLeft:-480}, 1000, function() {
-      $(this).find("li:last").after($(this).find("li:first"));
-      $(this).css({marginLeft:0});
-    })
-  }, 5000);
-})
+// $(document).ready(function() {
+//   var t = setInterval(function() {
+//     $("#carousel ul").animate({marginLeft:-480}, 1000, function() {
+//       $(this).find("li:last").after($(this).find("li:first"));
+//       $(this).css({marginLeft:0});
+//     })
+//   }, 5000);
+// })
+
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
